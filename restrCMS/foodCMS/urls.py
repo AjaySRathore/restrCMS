e2bb68a8f622fb5f255d7b1ee4d7e7a01f6a32a3
ajay <ajay.rathore.sr@gmail.com>
2020-06-24 18:29:39 +0300

    add details view

--- restrCMS/foodCMS/urls.py
+++ restrCMS/foodCMS/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('nutri-info-list/', views.NutritionInfoListView.as_view(),name='nutri-info-list'),
+    path('nutri-info-list/', views.NutritionInfoListView.as_view(),
+        name='nutri-info-list'),
+    path('nutri-details/<int:pk>',views.NutritionInfoDetailView.as_view(),name="nutri-details"),
+
 ]
--- restrCMS/foodCMS/views.py
+++ restrCMS/foodCMS/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from foodCMS.models import NutritionInfo,NutriDirectory,Products
 
@@ -8,3 +9,8 @@ from foodCMS.models import NutritionInfo,NutriDirectory,Products
 class NutritionInfoListView(ListView):
     model = NutritionInfo
     template_name = 'foodCMS/nutritionInfolist.html'
+
+@method_decorator(login_required, name="dispatch")
+class NutritionInfoDetailView(DetailView):
+    model = NutritionInfo
+    template_name = 'foodCMS/nutrition/nutritionDetailView.html'
